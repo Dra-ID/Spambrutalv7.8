1b773e1b0a67ccf20c780fec2de5f92af24b5a2b
vindra XD <104926748+Dra-ID@users.noreply.github.com>
2023-03-24 17:05:40 +0700

    Add files via upload

---
+++ module.sh
@@ -0,0 +1,14 @@
+pkg install python
+pkg install python2
+pkg install git
+pkg install nano
+gem install curl
+pkg install bash
+pip install requests
+pip install mechanize
+pip install lolcat
+pip install termcolor
+
+sleep 3
+
+python bot.py
